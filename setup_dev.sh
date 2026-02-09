8c65b5bc6cee8832843bb5c4dbf9951cc9559d37
Heorhi Marazenka <105611436+marazenka@users.noreply.github.com>
2026-02-09 20:56:20 +0100

    Install google-cloud-cli-gke-gcloud-auth-plugin
    Added installation for google-cloud-cli-gke-gcloud-auth-plugin.

--- setup_dev.sh
+++ setup_dev.sh
@@ -14,6 +14,7 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor 
 echo "deb [signed-by=/etc/apt/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt update
 sudo apt install -y google-cloud-cli
+sudo apt install -y google-cloud-cli-gke-gcloud-auth-plugin
 
 # 2. Kubectl v1.35
 echo "Installing Kubectl v1.35..."
